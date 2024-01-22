d7fc4ce13a02befe02265361eb94798b2107c727
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-22 16:13:43 -0600

    debugging hero image

--- portfolio-website/tailwind.config.js
+++ portfolio-website/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        hero: "url('./assets/background.png')",
+      },
+    },
   },
   plugins: [],
 };
