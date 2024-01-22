8b5cea74abda46c107ef9c48ab4debc9aa2c06e6
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-22 16:09:39 -0600

    working on landing page

--- portfolio-website/src/components/MainPage.jsx
+++ portfolio-website/src/components/MainPage.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import Background from "../assets/background.png";
 
 const MainPage = () => {
-  return <div>MainPage</div>;
+  return (
+    <>
+      <section className="text-gray-600 body-font">
+        <div class="container flex flex-wrap px-5 py-24 mx-auto items-center">
+          <div class="md:w-1/2 md:pr-12 md:py-8 md:border-r md:border-b-0 mb-10 md:mb-0 pb-10 border-b border-gray-200">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
+              Designing Innovative Websites
+            </h1>
+            <p class="leading-relaxed text-base">Hello, Hello, Hello</p>
+            <a class="text-indigo-500 inline-flex items-center mt-4">
+              Learn More
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-4 h-4 ml-2"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default MainPage;
