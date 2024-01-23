8f4555fdc6839914054c2a6c21ec4d408e79b6a8
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-23 14:16:09 -0600

    added type react animation to main page

--- portfolio-website/src/components/MainPage.jsx
+++ portfolio-website/src/components/MainPage.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 import Background from "../assets/background.png";
+import { TypeAnimation } from "react-type-animation";
 
 const MainPage = () => {
   return (
-    <div className="justify-center align-items">
-      <h1 className="absolute z-10 text-black text-4xl ml-30 mt-60 font-bold p-4">
-        Creating beautiful, user-friendly
-      </h1>
-      <h1 className="absolute z-10 text-black text-4xl ml-30 mt-80 font-bold p-4">
-        websites
-      </h1>
-      <img
-        loading="lazy"
-        src={Background}
-        className=" object-center w-full overflow-hidden my-auto max-md:max-w-full max-md:mt-10 rounded-lg"
-      />
+    <div className="flex flex-row justify-center align-items">
+      <div className="relative font-bold">
+        <TypeAnimation
+          sequence={[
+            // Same substring at the start will only be typed out once, initially
+            "Software Developer",
+            1000, // wait 1s before replacing "Mice" with "Hamsters"
+            "Coder",
+            1000,
+            "Designer",
+            1000,
+            "Creative",
+            1000,
+          ]}
+          wrapper="span"
+          speed={50}
+          style={{ fontSize: "2em", display: "inline-block" }}
+          repeat={Infinity}
+        />
+
+        <img
+          loading="lazy"
+          src={Background}
+          className=" object-center w-full overflow-hidden my-auto max-md:max-w-full max-md:mt-10 rounded-lg"
+        />
+      </div>
     </div>
   );
 };
