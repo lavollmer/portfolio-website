000a8b8a36078fec5ae8a27f870301efdcdb4046
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-23 13:26:57 -0600

    added hero image to main page

--- portfolio-website/src/components/MainPage.jsx
+++ portfolio-website/src/components/MainPage.jsx
@@ -3,22 +3,24 @@ import Background from "../assets/background.png";
 
 const MainPage = () => {
   return (
-    <>
-<div class="w-screen h-screen overflow-hidden relative before:block before:absolute before:bg-black before:h-full before:w-full before:top-0 before:left-0 before:z-10 before:opacity-30">
-  <img src="https://unsplash.com/photos/white-desk-lamp-beside-green-plant-BlIhVfXbi9s" class="absolute top-0 left-0 min-h-full ob" alt="">
-  <div class="relative z-20 max-w-screen-lg mx-auto grid grid-cols-12 h-full items-center">
-    <div class="col-span-6">
-      <span class="uppercase text-white text-xs font-bold mb-2 block">Welcome</span>
-      <h1 class="text-white font-extrabold text-5xl mb-8">Designing beautiful and user friendly websites</h1>
-      <p class="text-stone-100 text-base">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-      </p>
-      <button class="mt-8 text-white uppercase py-4 text-base font-light px-10 border border-white hover:bg-white hover:bg-opacity-10">Get started</button>
+    <div classname="bg-white flex flex-col justify-center items-stretch ">
+      <div classname="bg-neutral-50 flex w-full flex-col items-stretch pl-9 pr-4 pt-4 pb-12 max-md:max-w-full max-md:pl-5">
+        <div classname="flex w-full items-center justify-between gap-5 max-md:max-w-full max-md:flex-wrap">
+          <div classname="self-center w-full max-w-[1266px] mt-20 mb-12 max-md:max-w-full max-md:my-10">
+            <div classname="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+              <div classname="flex flex-col items-stretch w-[52%] max-md:w-full max-md:ml-0">
+                <img
+                  loading="lazy"
+                  src={Background}
+                  classname="aspect-[0.96] object-contain object-center w-full overflow-hidden my-auto max-md:max-w-full max-md:mt-10"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   );
 };
-      </>
 
 export default MainPage;
