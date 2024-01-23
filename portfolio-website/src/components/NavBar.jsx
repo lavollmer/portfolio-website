9666931e2a34803d0ca669da8f86772726b64e96
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-23 12:54:15 -0600

    finishing the header

--- portfolio-website/src/components/NavBar.jsx
+++ portfolio-website/src/components/NavBar.jsx
@@ -5,10 +5,10 @@ const NavBar = () => {
     <header class="text-gray-600 body-font">
       <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <nav class="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
-          <a class="mr-5 hover:text-gray-900">First Link</a>
-          <a class="mr-5 hover:text-gray-900">Second Link</a>
-          <a class="mr-5 hover:text-gray-900">Third Link</a>
-          <a class="hover:text-gray-900">Fourth Link</a>
+          <a class="mr-5 hover:text-gray-900">About</a>
+          <a class="mr-5 hover:text-gray-900">Projects</a>
+          <a class="mr-5 hover:text-gray-900">Resume</a>
+          <a class="hover:text-gray-900">Contact</a>
         </nav>
         <a class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
           <span class="ml-3 text-xl">Laura Vollmer, MBA</span>
