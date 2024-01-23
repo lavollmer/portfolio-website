27810fd9de610fb16815511c81f7a9a7ed88557e
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-23 13:09:51 -0600

    styling main page with tailwind CSS and hero image

--- portfolio-website/src/components/MainPage.jsx
+++ portfolio-website/src/components/MainPage.jsx
@@ -4,28 +4,17 @@ import Background from "../assets/background.png";
 const MainPage = () => {
   return (
     <>
-      <section className="text-gray-600 body-font">
-        <div class="container flex flex-wrap px-5 py-24 mx-auto items-center">
-          <div class="md:w-1/2 md:pr-12 md:py-8 md:border-r md:border-b-0 mb-10 md:mb-0 pb-10 border-b border-gray-200">
-            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
-              Designing Innovative Websites
-            </h1>
-            <p class="leading-relaxed text-base">Hello, Hello, Hello</p>
-            <a class="text-indigo-500 inline-flex items-center mt-4">
-              Learn More
-              <svg
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                class="w-4 h-4 ml-2"
-                viewBox="0 0 24 24"
-              >
-                <path d="M5 12h14M12 5l7 7-7 7"></path>
-              </svg>
-            </a>
-          </div>
+      <section className="text-gray-600 body-font w-full h-full relative">
+        <img
+          src={Background}
+          alt="Background Image"
+          className="rounded-radius absolute inset-0 w-full h-full object-cover"
+        />
+        <div class="md:w-1/2 md:pr-12 md:py-8 md:border-r md:border-b-0 mb-10 md:mb-0 pb-10 border-b border-gray-200 relative z-10">
+          <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
+            Designing Innovative Websites
+          </h1>
+          <p class="leading-relaxed text-base">Hello, Hello, Hello</p>
         </div>
       </section>
     </>
