01b025e0fcaf8b704053ded605bd2eb2aeda0e21
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-23 13:21:08 -0600

    debugging Main Page

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import About from "./components/About";
+
 import "./App.css";
 
 function App() {
--- portfolio-website/src/components/MainPage.jsx
+++ portfolio-website/src/components/MainPage.jsx
@@ -4,21 +4,21 @@ import Background from "../assets/background.png";
 const MainPage = () => {
   return (
     <>
-      <section className="text-gray-600 body-font w-full h-full relative">
-        <img
-          src={Background}
-          alt="Background Image"
-          className="rounded-radius absolute inset-0 w-full h-full object-cover"
-        />
-        <div class="md:w-1/2 md:pr-12 md:py-8 md:border-r md:border-b-0 mb-10 md:mb-0 pb-10 border-b border-gray-200 relative z-10">
-          <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
-            Designing Innovative Websites
-          </h1>
-          <p class="leading-relaxed text-base">Hello, Hello, Hello</p>
-        </div>
-      </section>
-    </>
+<div class="w-screen h-screen overflow-hidden relative before:block before:absolute before:bg-black before:h-full before:w-full before:top-0 before:left-0 before:z-10 before:opacity-30">
+  <img src="https://unsplash.com/photos/white-desk-lamp-beside-green-plant-BlIhVfXbi9s" class="absolute top-0 left-0 min-h-full ob" alt="">
+  <div class="relative z-20 max-w-screen-lg mx-auto grid grid-cols-12 h-full items-center">
+    <div class="col-span-6">
+      <span class="uppercase text-white text-xs font-bold mb-2 block">Welcome</span>
+      <h1 class="text-white font-extrabold text-5xl mb-8">Designing beautiful and user friendly websites</h1>
+      <p class="text-stone-100 text-base">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <button class="mt-8 text-white uppercase py-4 text-base font-light px-10 border border-white hover:bg-white hover:bg-opacity-10">Get started</button>
+    </div>
+  </div>
+</div>
   );
 };
+      </>
 
 export default MainPage;
