2afacc877d6e5cf6c523af4fcc7430dda5b1d9c9
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-23 12:53:32 -0600

    stylizing header with tailwindCSS and tailblocks

--- portfolio-website/src/components/NavBar.jsx
+++ portfolio-website/src/components/NavBar.jsx
@@ -2,31 +2,37 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <div className="flex justify-center items-center align-items space-x-4">
-      <div>
-        <a href="#about">
-          <h2>About</h2>
+    <header class="text-gray-600 body-font">
+      <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <nav class="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
+          <a class="mr-5 hover:text-gray-900">First Link</a>
+          <a class="mr-5 hover:text-gray-900">Second Link</a>
+          <a class="mr-5 hover:text-gray-900">Third Link</a>
+          <a class="hover:text-gray-900">Fourth Link</a>
+        </nav>
+        <a class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
+          <span class="ml-3 text-xl">Laura Vollmer, MBA</span>
         </a>
+        <div class="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
+          <a href="https://www.linkedin.com/in/lvollmer/">
+            <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+              LinkedIn
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-4 h-4 ml-1"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </button>
+          </a>
+        </div>
       </div>
-      <div>
-        <a href="#resume">
-          <h2>Resume</h2>
-        </a>
-      </div>
-      <div className="text-xl">
-        <h1>Laura Vollmer</h1>
-      </div>
-      <div>
-        <a href="#projects">
-          <h2>Projects</h2>
-        </a>
-      </div>
-      <div>
-        <a href="#contact">
-          <h2>Contact</h2>
-        </a>
-      </div>
-    </div>
+    </header>
   );
 };
 
