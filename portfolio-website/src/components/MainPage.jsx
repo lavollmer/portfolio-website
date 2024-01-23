63b26f50e6e35a6e2698476b52fc2b7efabe5d7b
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-23 13:32:13 -0600

    fixing image on main page

--- portfolio-website/src/components/MainPage.jsx
+++ portfolio-website/src/components/MainPage.jsx
@@ -3,22 +3,12 @@ import Background from "../assets/background.png";
 
 const MainPage = () => {
   return (
-    <div classname="bg-white flex flex-col justify-center items-stretch ">
-      <div classname="bg-neutral-50 flex w-full flex-col items-stretch pl-9 pr-4 pt-4 pb-12 max-md:max-w-full max-md:pl-5">
-        <div classname="flex w-full items-center justify-between gap-5 max-md:max-w-full max-md:flex-wrap">
-          <div classname="self-center w-full max-w-[1266px] mt-20 mb-12 max-md:max-w-full max-md:my-10">
-            <div classname="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-              <div classname="flex flex-col items-stretch w-[52%] max-md:w-full max-md:ml-0">
-                <img
-                  loading="lazy"
-                  src={Background}
-                  classname="aspect-[0.96] object-contain object-center w-full overflow-hidden my-auto max-md:max-w-full max-md:mt-10"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className=" flex flex-row justify-center align-items ">
+      <img
+        loading="lazy"
+        src={Background}
+        className=" object-center w-full overflow-hidden my-auto max-md:max-w-full max-md:mt-10 rounded-lg"
+      />
     </div>
   );
 };
