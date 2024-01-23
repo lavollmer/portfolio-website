eddbbddbfc7183aae94fec16cca0ead78a5ee8d8
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-23 12:48:06 -0600

    creation of contact form with tailwindCSS and tailblocks

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -2,6 +2,7 @@ import MainPage from "./components/MainPage";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <NavBar />
       <MainPage />
       <Projects />
+      <Contact />
       <Footer />
     </>
   );
