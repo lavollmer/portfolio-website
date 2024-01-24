65df41e5c020336b9f04f2bbf3cfb9f3b59ea94d
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-24 16:06:10 -0600

    finished styling the paragraph of information in About section

--- portfolio-website/src/components/About.jsx
+++ portfolio-website/src/components/About.jsx
@@ -10,6 +10,7 @@ const About = () => {
             <h2 className="text-gray-900 font-large font-bold title-font tracking-wider text-md ">
               ABOUT
             </h2>
+            <br></br>
             <p className="text-lg">
               I am a full stack developer and healthcare professional with a
               passion for technology and web development. With 5-6 years of
@@ -17,15 +18,22 @@ const About = () => {
               and marketing through my position as a floral designer and social
               media coordinator at a small business flower shop, I am driven by
               problem-solving and the ever-expanding realm of technology.
+            </p>
+            <br></br>
+            <p className="text-lg">
               Specializing in front-end and back-end web development, I combine
               technical skills with a keen eye for design to deliver exceptional
               user experiences. With recent proficiency in JavaScript, ReactJS,
               HTML, Chakra UI, Bootstrap, and CSS, I am focused on becoming a
               proficient web developer and creating innovative, visually
-              captivating websites. I eagerly seek opportunities to connect,
-              network, and collaborate with like-minded individuals, fostering a
-              community of growth and learning while exploring new avenues for
-              educational and professional development.
+              captivating websites.
+            </p>
+            <br></br>
+            <p className="text-lg">
+              I eagerly seek opportunities to connect, network, and collaborate
+              with like-minded individuals, fostering a community of growth and
+              learning while exploring new avenues for educational and
+              professional development.
             </p>
             <span className="inline-block h-1 w-10 rounded bg-black mt-8 mb-6"></span>
             <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm">
