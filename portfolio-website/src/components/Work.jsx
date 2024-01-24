5b586d84b39b7fa1405933828755532c74000b27
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-24 16:34:17 -0600

    styling work list

--- portfolio-website/src/components/Work.jsx
+++ portfolio-website/src/components/Work.jsx
@@ -27,15 +27,50 @@ const Work = () => {
             <div class="py-8 flex flex-wrap md:flex-nowrap">
               <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
                 <span class="font-semibold title-font text-black text-left">
-                  University of Minnesota Coding Full Stack Bootcamp Student
+                  Floral Designer
                 </span>
                 <span class="mt-3 text-gray-500 text-sm text-left">
-                  Sep 2023 - Dec 2023
+                  Sep 2022 - June 2023
                 </span>
               </div>
               <div class="md:flex-grow">
                 <p class="leading-relaxed ml-4 text-left">
-                  <li>Learned about CSS, HTML</li>
+                  <li>
+                    Developed a comprehensive Instagram social media strategy
+                    with a small business account that included increasing
+                    followers, creating high-quality content and driving
+                    engagement (i.e., February 2023 results: created 131
+                    Instagram content pieces, reached 1,558 accounts (+79%
+                    increase), increased followers by 11.3%, 6,600 impressions,
+                    and 143 profile visits)
+                  </li>
+                  <li>
+                    Published 400+ floral designs, behind the scenes and shop
+                    related Instagram content with high-quality images and
+                    videos (utilizing iPhone, Adobe Lightroom, Canva graphics,
+                    Facebook Business Meta Suite, personal content calendar,
+                    etc.)
+                  </li>
+                  <li>
+                    Entered 500+ customers’ data into floral industry-specific
+                    software with exceptional accuracy and fast typing for
+                    delivery, pricing and customer satisfaction
+                  </li>
+                  <li>
+                    Communicated with 1,000+ customers via phone, online or
+                    in-person on ordering, floral design, delivery, purchasing,
+                    and general business questions
+                  </li>
+                  <li>
+                    Provided professional and courteous customer service to
+                    1,000+ customers (corporations, funeral homes, schools,
+                    venues, individuals, etc.)
+                  </li>
+                  <li>
+                    Acquired comprehensive knowledge, training and skills in
+                    floral design through independent study and hands-on
+                    experience
+                  </li>
                 </p>
               </div>
             </div>
