fe53e2a1788c57e046b21176b098aeb82bcf177b
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-25 12:19:54 -0600

    fixing the header

--- portfolio-website/src/components/NavBar.jsx
+++ portfolio-website/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <header className="text-gray-600 body-font">
+    <header className="flex text-gray-600 body-font space-x-5">
       <div className="container mx-auto w-full h-full flex flex-wrap p-5 flex-col md:flex-row items-center">
         <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
           <a className="mr-5 hover:text-gray-900" href="#about">
@@ -26,10 +26,26 @@ const NavBar = () => {
             Laura Vollmer, MBA
           </span>
         </a>
-        <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
+        <div className="flex lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0 space-x-5">
           <a href="https://www.linkedin.com/in/lvollmer/">
             <button className="inline-flex items-center bg-[#838871] text-white border-0 py-1 px-3 focus:outline-none hover:bg-[#BAB7B2] rounded text-base mt-4 md:mt-0">
               LinkedIn
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                className="w-4 h-4 ml-1"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </button>
+          </a>
+          <a href="https://github.com/lavollmer">
+            <button className="inline-flex items-center bg-[#838871] text-white border-0 py-1 px-3 focus:outline-none hover:bg-[#BAB7B2] rounded text-base mt-4 md:mt-0">
+              Github
               <svg
                 fill="none"
                 stroke="currentColor"
