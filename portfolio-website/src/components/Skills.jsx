abb7130371fd1892563b8bb73b8ffe5e76348a9d
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-25 11:45:09 -0600

    updating styling of skills button

--- portfolio-website/src/components/Skills.jsx
+++ portfolio-website/src/components/Skills.jsx
@@ -22,7 +22,7 @@ const Skills = () => {
           <div class="flex flex-wrap -m-4">
             <div class="xl:w-1/3 md:w-1/2 p-4">
               <div class="border border-gray-200 p-6 rounded-lg">
-                <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-[#838871] text-white mb-4">
                   <IoLogoJavascript />
                 </div>
                 <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
@@ -36,7 +36,7 @@ const Skills = () => {
             </div>
             <div class="xl:w-1/3 md:w-1/2 p-4">
               <div class="border border-gray-200 p-6 rounded-lg">
-                <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-[#838871] text-white mb-4">
                   {/* <svg
                     fill="none"
                     stroke="currentColor"
@@ -63,7 +63,7 @@ const Skills = () => {
             </div>
             <div class="xl:w-1/3 md:w-1/2 p-4">
               <div class="border border-gray-200 p-6 rounded-lg">
-                <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-[#838871] text-white mb-4">
                   {/* <svg
                     fill="none"
                     stroke="currentColor"
@@ -89,7 +89,7 @@ const Skills = () => {
             </div>
             <div class="xl:w-1/3 md:w-1/2 p-4">
               <div class="border border-gray-200 p-6 rounded-lg">
-                <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-[#838871] text-white mb-4">
                   {/* <svg
                     fill="none"
                     stroke="currentColor"
@@ -114,7 +114,7 @@ const Skills = () => {
             </div>
             <div class="xl:w-1/3 md:w-1/2 p-4">
               <div class="border border-gray-200 p-6 rounded-lg">
-                <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-[#838871] text-white mb-4">
                   {/* <svg
                     fill="none"
                     stroke="currentColor"
@@ -139,7 +139,7 @@ const Skills = () => {
             </div>
             <div class="xl:w-1/3 md:w-1/2 p-4">
               <div class="border border-gray-200 p-6 rounded-lg">
-                <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-[#838871] text-white mb-4">
                   {/* <svg
                     fill="none"
                     stroke="currentColor"
@@ -163,9 +163,14 @@ const Skills = () => {
               </div>
             </div>
           </div>
-          <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-            Button
-          </button>
+          <a
+            href="https://github.com/lavollmer"
+            className="flex flex-col justify-center items-center mt-16"
+          >
+            <button class="flex mt-16 text-white bg-black border-0 py-2 px-8 focus:outline-none hover:bg-gray-500 rounded text-lg">
+              Github
+            </button>
+          </a>
         </div>
       </section>
     </div>
