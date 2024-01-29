3ff4926befe17815c2530d7c5a3397b54a20db87
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-29 12:47:00 -0600

    added more projects and images to the website

--- portfolio-website/src/components/Projects.jsx
+++ portfolio-website/src/components/Projects.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import Color from "../assets/colorimage.png";
 import Robo from "../assets/roboguesswho.png";
 import Hex from "../assets/hexcode.png";
 import Gift from "../assets/giftify.png";
+import Tribute from "../assets/TributeCover.png";
+import SQL from "../assets/sqlemployee.png";
+import Note from "../assets/note.png";
 
 const Projects = () => {
   return (
     <>
-      <section className="text-gray-600 body-font" id="projects">
+      <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-col text-center w-full mb-20">
+          <div className="flex flex-col text-center w-full mb-20" id="projects">
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
               Projects
             </h1>
@@ -97,12 +99,12 @@ const Projects = () => {
                 </div>
               </div>
             </div>
-            {/* <div className="lg:w-1/3 sm:w-1/2 p-4">
+            <div className="lg:w-1/3 sm:w-1/2 p-4">
               <div className="flex relative">
                 <img
-                  alt="gallery"
                   className="absolute inset-0 w-full h-full object-cover object-center"
-                  src="https://dummyimage.com/602x362"
+                  src={Tribute}
+                  alt="Grace Hopper Information Page"
                 />
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
@@ -123,21 +125,20 @@ const Projects = () => {
             <div className="lg:w-1/3 sm:w-1/2 p-4">
               <div className="flex relative">
                 <img
-                  alt="gallery"
                   className="absolute inset-0 w-full h-full object-cover object-center"
-                  src="https://dummyimage.com/605x365"
+                  src={Note}
+                  alt="Note Taking Application"
                 />
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
                     Solo Project
                   </h2>
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                    Note Taker
+                    Note Taking Application
                   </h1>
                   <p className="leading-relaxed">
                     The goal of the project is to create a js file that connects
-                    the front-end with the back-end functionality. The note
-                    taker application opens with a entry screen. When the user
+                    the front-end with the back-end functionality. When the user
                     clicks "get started", they are brought to an area to take
                     notes, save notes and delete the notes.
                   </p>
@@ -147,9 +148,9 @@ const Projects = () => {
             <div className="lg:w-1/3 sm:w-1/2 p-4">
               <div className="flex relative">
                 <img
-                  alt="gallery"
                   className="absolute inset-0 w-full h-full object-cover object-center"
-                  src="https://dummyimage.com/606x366"
+                  src={SQL}
+                  alt="SQL Employee Tracker"
                 />
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
@@ -162,13 +163,11 @@ const Projects = () => {
                     This project involved creating a command line user prompt to
                     store data about a business. The inquirer prompt asked the
                     end user to view all departments, view all roles, view all
-                    employees, add an employee, remove an employee, update and
-                    employee, etc. The end user is able to change the data based
-                    on the request and update the printed table of data.
+                    employees, add an employee, etc.
                   </p>
                 </div>
               </div>
-            </div> */}
+            </div>
           </div>
         </div>
       </section>
