ebb6f9ab377c55c9d0e020f8aa0de5564dc7c104
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-29 15:12:28 -0600

    working on contact form and syncing emailJS

--- portfolio-website/src/components/Projects.jsx
+++ portfolio-website/src/components/Projects.jsx
@@ -106,12 +106,12 @@ const Projects = () => {
             <div className="lg:w-1/3 sm:w-1/2 p-4">
               <div className="flex relative">
                 <img
-                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  className="absolute inset-0 w-full h-full object-cover object-center rounded-md"
                   src={Tribute}
                   alt="Grace Hopper Information Page"
                 />
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-[#625C47] mb-1">
+                  <h2 className="tracking-widest text-sm title-font font-medium text-[#625C47] mb-1 rounded-md">
                     Solo Project
                   </h2>
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
@@ -129,12 +129,12 @@ const Projects = () => {
             <div className="lg:w-1/3 sm:w-1/2 p-4">
               <div className="flex relative">
                 <img
-                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  className="absolute inset-0 w-full h-full object-cover object-center rounded-md"
                   src={Note}
                   alt="Note Taking Application"
                 />
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-[#625C47] mb-1">
+                  <h2 className="tracking-widest text-sm title-font font-medium text-[#625C47] mb-1 ">
                     Solo Project
                   </h2>
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
@@ -152,7 +152,7 @@ const Projects = () => {
             <div className="lg:w-1/3 sm:w-1/2 p-4">
               <div className="flex relative">
                 <img
-                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  className="absolute inset-0 w-full h-full object-cover object-center rounded-md"
                   src={SQL}
                   alt="SQL Employee Tracker"
                 />
