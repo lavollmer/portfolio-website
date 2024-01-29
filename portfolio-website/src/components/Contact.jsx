4279f8239bf69b8249b31f41864a5a088e497899
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-29 15:50:57 -0600

    functioning emailJS

--- portfolio-website/src/components/Contact.jsx
+++ portfolio-website/src/components/Contact.jsx
@@ -1,19 +1,24 @@
-import React from "react";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 //import emailjs from emailjs/browser package
 import emailjs from "@emailjs/browser";
+
+// //intialize emailJS
+// init("service_qkcv0rh");
 
 //define functional component Contact
 const Contact = () => {
   //useRef hook to reference variable form
   const form = useRef();
 
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
   //handle form submission
   const sendEmail = (e) => {
     //stop form submission default behavior
     e.preventDefault();
-    //form.current represents the data to be sent
     emailjs
+      //form.current represents the data to be sent
+      //service ID, template ID, and public key
       .sendForm(
         "service_qkcv0rh",
         "template_r8nnexl",
@@ -23,6 +28,8 @@ const Contact = () => {
       .then(
         (result) => {
           console.log(result.text);
+          //set state to show successful submission
+          setIsSubmitted(true);
         },
         //catch errors
         (error) => {
@@ -63,68 +70,82 @@ const Contact = () => {
             </div>
           </div>
           {/* onSubmit is set to sendEmail which is triggered when form is submitted, form data is called and sent with emailJS */}
-          <form ref={form} onSubmit={sendEmail}>
-            <div className="lg:w-1/2 md:w-2/3 mx-auto">
-              <div className="flex flex-wrap -m-2">
-                <div className="p-2 w-1/2">
-                  <div className="relative">
-                    <label
-                      htmlFor="name"
-                      className="leading-7 text-sm text-gray-600"
-                    >
-                      Name
-                    </label>
-                    <input
-                      type="text"
-                      id="name"
-                      name="name"
-                      className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                    />
-                  </div>
-                </div>
-                <div className="p-2 w-1/2">
-                  <div className="relative">
-                    <label
-                      htmlFor="email"
-                      className="leading-7 text-sm text-gray-600"
-                    >
-                      Email
-                    </label>
-                    <input
-                      type="email"
-                      id="email"
-                      name="email"
-                      className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                    />
-                  </div>
-                </div>
-                <div className="p-2 w-full">
-                  <div className="relative">
-                    <label
-                      htmlFor="message"
-                      className="leading-7 text-sm text-gray-600"
-                    >
-                      Message
-                    </label>
-                    <textarea
-                      id="message"
-                      name="message"
-                      className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                    ></textarea>
-                  </div>
-                </div>
-                <div className="p-2 w-full">
-                  <button
-                    className="flex mx-auto text-white bg-black border-0 py-2 px-8 focus:outline-none hover:bg-gray-500 rounded text-lg"
-                    type="submit"
-                    value="Send"
-                  >
-                    Submit
-                  </button>
-                </div>
+          <div>
+            {isSubmitted ? (
+              <div className="flex flex-col justify-center items-center border-2 border-green p-5">
+                <p className="text-green font-bold ">
+                  Form successfully submitted!
+                </p>
+                <p>
+                  We will be in contact with you or your company in 1-2 business
+                  days.
+                </p>
               </div>
-            </div>
-          </form>
+            ) : (
+              <form ref={form} onSubmit={sendEmail}>
+                <div className="lg:w-1/2 md:w-2/3 mx-auto">
+                  <div className="flex flex-wrap -m-2">
+                    <div className="p-2 w-1/2">
+                      <div className="relative">
+                        <label
+                          htmlFor="name"
+                          className="leading-7 text-sm text-gray-600"
+                        >
+                          Name
+                        </label>
+                        <input
+                          type="text"
+                          id="name"
+                          name="name"
+                          className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        />
+                      </div>
+                    </div>
+                    <div className="p-2 w-1/2">
+                      <div className="relative">
+                        <label
+                          htmlFor="email"
+                          className="leading-7 text-sm text-gray-600"
+                        >
+                          Email
+                        </label>
+                        <input
+                          type="email"
+                          id="email"
+                          name="email"
+                          className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        />
+                      </div>
+                    </div>
+                    <div className="p-2 w-full">
+                      <div className="relative">
+                        <label
+                          htmlFor="message"
+                          className="leading-7 text-sm text-gray-600"
+                        >
+                          Message
+                        </label>
+                        <textarea
+                          id="message"
+                          name="message"
+                          className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                        ></textarea>
+                      </div>
+                    </div>
+                    <div className="p-2 w-full">
+                      <button
+                        className="flex mx-auto text-white bg-black border-0 py-2 px-8 focus:outline-none hover:bg-gray-500 rounded text-lg"
+                        type="submit"
+                        value="Send"
+                      >
+                        Submit
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            )}
+          </div>
         </div>
       </section>
     </>
