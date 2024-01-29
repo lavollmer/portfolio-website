edea035951bb9ce6b1465c3539862da94ec4bf0b
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-29 15:23:29 -0600

    adding JS functionality to send email with emailJS

--- portfolio-website/src/components/Contact.jsx
+++ portfolio-website/src/components/Contact.jsx
@@ -1,7 +1,31 @@
 import React from "react";
-import { FaLocationDot } from "react-icons/fa6";
+import React, { useRef } from "react";
+//import emailjs from emailjs/browser package
+import emailjs from "@emailjs/browser";
 
+//define functional component Contact
 const Contact = () => {
+  //useRef hook to reference variable form
+  const form = useRef();
+
+  //handle form submission
+  const sendEmail = (e) => {
+    //stop form submission default behavior
+    e.preventDefault();
+    //form.current represents the data to be sent
+    emailjs
+      .sendForm("SERVICE ID", "TEMPLATE ID", form.current, "public key")
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        //catch errors
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <>
       <section className=" flex content-center text-gray-600 body-font relative">
@@ -33,62 +57,69 @@ const Contact = () => {
               </p>
             </div>
           </div>
-          <div className="lg:w-1/2 md:w-2/3 mx-auto">
-            <div className="flex flex-wrap -m-2">
-              <div className="p-2 w-1/2">
-                <div className="relative">
-                  <label
-                    htmlFor="name"
-                    className="leading-7 text-sm text-gray-600"
-                  >
-                    Name
-                  </label>
-                  <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                  />
+          {/* onSubmit is set to sendEmail which is triggered when form is submitted, form data is called and sent with emailJS */}
+          <form ref={form} onSubmit={sendEmail}>
+            <div className="lg:w-1/2 md:w-2/3 mx-auto">
+              <div className="flex flex-wrap -m-2">
+                <div className="p-2 w-1/2">
+                  <div className="relative">
+                    <label
+                      htmlFor="name"
+                      className="leading-7 text-sm text-gray-600"
+                    >
+                      Name
+                    </label>
+                    <input
+                      type="text"
+                      id="name"
+                      name="name"
+                      className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="p-2 w-1/2">
-                <div className="relative">
-                  <label
-                    htmlFor="email"
-                    className="leading-7 text-sm text-gray-600"
-                  >
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                  />
+                <div className="p-2 w-1/2">
+                  <div className="relative">
+                    <label
+                      htmlFor="email"
+                      className="leading-7 text-sm text-gray-600"
+                    >
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    />
+                  </div>
                 </div>
-              </div>
-              <div className="p-2 w-full">
-                <div className="relative">
-                  <label
-                    htmlFor="message"
-                    className="leading-7 text-sm text-gray-600"
-                  >
-                    Message
-                  </label>
-                  <textarea
-                    id="message"
-                    name="message"
-                    className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-                  ></textarea>
+                <div className="p-2 w-full">
+                  <div className="relative">
+                    <label
+                      htmlFor="message"
+                      className="leading-7 text-sm text-gray-600"
+                    >
+                      Message
+                    </label>
+                    <textarea
+                      id="message"
+                      name="message"
+                      className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                    ></textarea>
+                  </div>
                 </div>
-              </div>
-              <div className="p-2 w-full">
-                <button className="flex mx-auto text-white bg-black border-0 py-2 px-8 focus:outline-none hover:bg-gray-500 rounded text-lg">
-                  Submit
-                </button>
+                <div className="p-2 w-full">
+                  <button
+                    className="flex mx-auto text-white bg-black border-0 py-2 px-8 focus:outline-none hover:bg-gray-500 rounded text-lg"
+                    type="submit"
+                    value="Send"
+                  >
+                    Submit
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
+          </form>
         </div>
       </section>
     </>
