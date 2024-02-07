4ebb275212ff31777d72159194967f7d82aae5c4
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-02-07 15:13:04 -0600

    updating work experience and styling

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -52,33 +52,31 @@ const Work = () => {
                   Floral Designer
                 </span>
                 <span className="mt-3 text-gray-500 text-sm text-left">
-                  Sep 2022 - June 2023
+                  2022 - 2023
                 </span>
               </div>
               <div className="md:flex-grow">
                 <ul>
                   <p className="leading-relaxed ml-4 text-left">
                     <li className="list-disc list-inside">
-                      Expertise in Visual Design: Developed a comprehensive
-                      Instagram social media strategy with a small business
-                      account, creating high-quality floral content to drive
-                      engagement and increase followers.Acquired comprehensive
-                      knowledge and skills in floral design, bringing an eye for
-                      aesthetics and attention to detail to web design projects.
+                      Effectively communicated with a diverse customer base of
+                      over a thousand individuals through various channels with
+                      a commitment to providing exceptional customer service
                     </li>
                     <li className="list-disc list-inside">
-                      Flawless Content Creation: Published hundreds of floral
-                      designs and related content on Instagram, utilizing
-                      high-quality images and videos to showcase creativity and
-                      craftsmanship.{" "}
+                      Entered customer data for industry-specific software with
+                      exceptional accuracy, including documentation and
+                      troubleshooting of software issues, ensuring concise
+                      information for over five hundred customers
                     </li>
                     <li className="list-disc list-inside">
-                      Data Entry and Accuracy: Entered customer data into
-                      industry-specific software with exceptional accuracy,
-                      ensuring seamless integration between floral design and
-                      web systems.
+                      Showcased visually appealing design proficiency by
+                      collaborating, employing critical thinking, and leveraging
+                      technical skills to publish 400+ high-quality floral
+                      designs, behind-the-scenes content, and shop-related
+                      social media posts
                     </li>
-                    <li className="list-disc list-inside">
+                    {/* <li className="list-disc list-inside">
                       Exceptional Customer Service: Communicated with a wide
                       range of customers on ordering, design, and general
                       business questions, providing professional and courteous
@@ -89,7 +87,7 @@ const Work = () => {
                       designers and developers to seamlessly integrate frontend
                       and backend components, ensuring optimal user experience
                       across different devices and screen sizes.
-                    </li>
+                    </li> */}
                   </p>
                 </ul>
               </div>
@@ -100,36 +98,35 @@ const Work = () => {
                   Manager of Clinical Content
                 </span>
                 <span className="mt-3 text-gray-500 text-sm text-left">
-                  Apr 2020 - Aug 2021
+                  2020 - 2021
                 </span>
               </div>
               <div className="md:flex-grow">
                 <ul>
                   <p className="leading-relaxed ml-4 text-left">
                     <li className="list-disc list-inside">
-                      Provided up-to-date and accurate protocol recommendations
-                      for healthfinch software by monitoring monthly changes in
-                      1,000+ medication profiles such as clinical guidelines,
-                      federal screening recommendations, FDA drug
-                      changes/approvals
+                      Demonstrated a strong work ethic by methodically
+                      monitoring monthly changes in over 1,000 medication
+                      profiles and providing accurate protocol recommendations
+                      and documentation for software
                     </li>
                     <li className="list-disc list-inside">
-                      Collaborated with 20+ interdisciplinary team members such
-                      as customer success managers, technical support managers,
-                      software engineers, sales team and physicians to provide
-                      safe and effective medication and lab monitoring
-                      recommendations for patients utilized in software
+                      Applied collaborative and critical thinking skills to
+                      demonstrate strong research and drug information
+                      expertise, ensuring accurate, evidence-based, and
+                      high-quality content for diverse audiences
                     </li>
                     <li className="list-disc list-inside">
-                      Demonstrated strong research, and drug information skills
-                      for content accuracy, evidence-based recommendations, and
-                      editorial content for all audiences
+                      Effectively collaborated with a team of over twenty
+                      interdisciplinary members, including physicians, customer
+                      success managers, technical support managers, software
+                      engineers, and sales representatives
                     </li>
-                    <li className="list-disc list-inside">
+                    {/* <li className="list-disc list-inside">
                       Supported client-facing meetings regarding protocol
                       consensus for software implementation at health- systems
                       around the United States
-                    </li>
+                    </li> */}
                   </p>
                 </ul>
               </div>
@@ -140,25 +137,57 @@ const Work = () => {
                   Clinical Content Reviewer
                 </span>
                 <span className="mt-3 text-gray-500 text-sm text-left">
-                  Nov 2019 - Apr 2020
+                  2019 - 2020
                 </span>
               </div>
               <div className="md:flex-grow">
                 <ul>
                   <p className="leading-relaxed ml-4 text-left">
                     <li className="list-disc list-inside">
-                      Reviewed, proofread and updated 1,000+ written online
-                      articles for exceptional accuracy, scientific literature
-                      citations, and editorial content for all audiences
+                      Ensured accuracy, scientific citation adherence, and
+                      suitability for all audiences by reviewing and revising
+                      over one thousand online articles
                     </li>
                     <li className="list-disc list-inside">
-                      Served as subject matter expert on clinical and healthcare
-                      topics for written content and content integrity
+                      Met all timely deadline requirements for the return of
+                      revised articles for review and publication
                     </li>
                     <li className="list-disc list-inside">
-                      Supported the preparation of clinical documents that are
-                      of high quality and prepared within timelines audiences
-                      (clinical, technical and non-technical professions)
+                      Documented hundreds of citation corrections to articles
+                      with attention to detail and content integrity
+                    </li>
+                  </p>
+                </ul>
+              </div>
+            </div>
+            <div className="py-8 flex flex-wrap md:flex-nowrap">
+              <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                <span className="font-semibold title-font text-black text-left">
+                  Clinical Pharmacy Specialist – Medication Use
+                </span>
+                <span className="mt-3 text-gray-500 text-sm text-left">
+                  2017 - 2019
+                </span>
+              </div>
+              <div className="md:flex-grow">
+                <ul>
+                  <p className="leading-relaxed ml-4 text-left">
+                    <li className="list-disc list-inside">
+                      Developed, wrote, and implemented multiple policies,
+                      procedures, and best practices to optimize medication
+                      management within the health system and associated
+                      hospitals
+                    </li>
+                    <li className="list-disc list-inside">
+                      Ensured compliance with external regulatory agencies,
+                      state laws, and regulations about patient care maintenance
+                      and prescription management.
+                    </li>
+                    <li className="list-disc list-inside">
+                      Educated and mentored 24 fourth-year pharmacy students,
+                      60+ staff members, 100+ second-year medical students, and
+                      10 pharmacy interns through rotations, clinical topic
+                      discussions, and medical literature journal clubs
                     </li>
                   </p>
                 </ul>
