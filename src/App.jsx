cd0bc69fc32e9126087df91fc75756692df8f7dd
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-02-08 14:22:31 -0600

    updating side menu for mobile and web design responsive

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Contact from "./components/Contact";
 import About from "./components/About";
 import Work from "./components/Work";
 import Skills from "./components/Skills";
+import Header from "./components/Header";
 
 import "./App.css";
 
@@ -13,6 +14,7 @@ function App() {
   return (
     <>
       <NavBar />
+      <Header />
       <MainPage />
       <About />
       <Skills />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 import HamburgerIcon from "../components/HamburgerIcon.jsx";
 
 const NavBar = () => {
+  //isMenuOpen default useState is set to fale and create setIsMenuOpen function
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  //when toggleMenu is called, it uses setIsMenuOpen function to update the value of isMenuOpen to the opposite of its current value using the logical operator of not(!)
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
@@ -12,12 +14,14 @@ const NavBar = () => {
     <>
       <header className="flex text-gray-600 body-font space-x-5">
         <div className="container mx-auto w-full h-full flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <HamburgerIcon onClick={toggleMenu} />
           <nav
-            className={`menu ${
-              isMenuOpen ? "block" : "hidden"
-            } bg-gray-800 text-white p-4 flex flex-col items-center`}
+            // if isMenuOpen is true then will display block otherwise will be hidden via TailwindCSS code
+            className={`fixed top-0 left-0 h-screen bg-gray-800 w-64 ${
+              isMenuOpen ? "translate-x-0" : "-translate-x-full"
+            } transition-transform duration-300 ease-in-out`}
           >
+            {/* when the hamburger icon is clicked on it will call the toggleMenu function */}
+            <HamburgerIcon onClick={toggleMenu} />
             <a className="my-2" href="#about">
               About
             </a>
@@ -34,7 +38,7 @@ const NavBar = () => {
               Contact
             </a>
           </nav>
-          <span className="ml-3 p-2 text-3xl border-solid font-family-arial text-black font-bold">
+          {/* <span className="ml-3 p-2 text-3xl border-solid font-family-arial text-black font-bold">
             Laura Vollmer
           </span>
           <div className="flex p-1 lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0 space-x-5">
@@ -70,7 +74,7 @@ const NavBar = () => {
                 </svg>
               </button>
             </a>
-          </div>
+          </div> */}
         </div>
       </header>
     </>
