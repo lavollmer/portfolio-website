132c475fddf043b7af137a38d2c2424de23ee1f9
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-03-06 10:36:51 -0600

    updating main page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Contact from "./components/Contact";
 import About from "./components/About";
 import Work from "./components/Work";
 import Skills from "./components/Skills";
+import ColorBackground from "./assets/ColorBackground.jpg";
+import NavBar from "./components/NavBar";
 
 import "./App.css";
 
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -3,16 +3,14 @@ import Background from "../assets/background.png";
 import { TypeAnimation } from "react-type-animation";
 import ScrollToTop from "react-scroll-to-top";
 import ColorBackground from "../assets/ColorBackground.jpg";
-import NavBar from "./NavBar";
 
 const MainPage = () => {
   return (
     <section id="home" className="min-h-screen">
-      <NavBar />
       <img
         loading="lazy"
         src={ColorBackground}
-        className="z-0 object-cover w-screen h-screen rounded-lg"
+        className="z-1 object-cover w-screen h-screen"
         // overflow-hidden my-auto max-md:max-w-full max-md:mt-10 rounded-lg"
         alt="Image of a pink and black splash background"
       />
