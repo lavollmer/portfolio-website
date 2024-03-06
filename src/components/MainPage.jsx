46683e2251d5914fcfdb1c9ba1bbb29b8e8cb5f6
Laura Vollmer <lauravollmer@Lauras-MacBook-Pro-2.local>
2024-03-06 17:36:17 -0600

    styling landing page

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -6,12 +6,12 @@ import { FaDivide } from "react-icons/fa";
 const MainPage = () => {
   return (
     <div id="home" className="main-page">
-      <div className="absolute top-1/4 left-1/4 transform -translate-y-1/2 -translate-x-1/4 z-10 text-black  font-bold p-4">
-        <h1 className="text-4xl font-poppins">
+      <div className="absolute top-1/3 left-1/4 transform -translate-y-1/2 -translate-x-1/4 z-10 text-black  p-4">
+        <h1 className="text-8xl font-poppins">
           Laura <br /> Vollmer
         </h1>
       </div>
-      <div className="absolute top-1/2 left-1/4 transform -translate-y-1/2 -translate-x-1/4 z-10 text-black text-2xl font-bold p-4">
+      {/* <div className="absolute top-1/2 left-1/4 transform -translate-y-1/2 -translate-x-1/4 z-10 text-black text-2xl  p-4">
         <h1>I am a</h1>
         <TypeAnimation
           sequence={[
@@ -29,7 +29,7 @@ const MainPage = () => {
           style={{ fontSize: "2em", display: "inline-block" }}
           repeat={Infinity}
         /> 
-       </div>
+       </div> */}
 
       <ScrollToTop smooth />
     </div>
