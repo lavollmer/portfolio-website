8bd9052a18487dc7a2c5acbea6184a86b0bb28ec
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-03-06 10:22:45 -0600

    fixing the main page formatting and styling

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -7,33 +7,33 @@ import ColorBackground from "../assets/ColorBackground.jpg";
 const MainPage = () => {
   return (
     <section id="home">
-      <div className="flex justify-center items-center">
-        <img
-          loading="lazy"
-          src={ColorBackground}
-          className="z-0 object-center w-full h-full overflow-hidden my-auto max-md:max-w-full max-md:mt-10 rounded-lg"
-          alt="Image of a desk and clock"
+      <img
+        loading="lazy"
+        src={ColorBackground}
+        className="z-0 object-cover w-screen h-screen rounded-lg"
+        // overflow-hidden my-auto max-md:max-w-full max-md:mt-10 rounded-lg"
+        alt="Image of a pink and black splash background"
+      />
+      <div className="absolute top-1/2 left-1/4 transform -translate-y-1/2 -translate-x-1/4 z-10 text-black text-2xl font-bold p-4">
+        <h1>I am a</h1>
+        <TypeAnimation
+          sequence={[
+            "Creative",
+            2000,
+            "<coder>",
+            2000,
+            "Designer",
+            2000,
+            "Web Developer",
+            2000,
+          ]}
+          wrapper="span"
+          speed={50}
+          style={{ fontSize: "2em", display: "inline-block" }}
+          repeat={Infinity}
         />
-        <div className="absolute top-1/2 left-1/4 transform -translate-y-1/2 -translate-x-1/4 z-10 text-black text-2xl font-bold p-4">
-          <h1>I am a</h1>
-          <TypeAnimation
-            sequence={[
-              "Creative",
-              2000,
-              "<coder>",
-              2000,
-              "Designer",
-              2000,
-              "Web Developer",
-              2000,
-            ]}
-            wrapper="span"
-            speed={50}
-            style={{ fontSize: "2em", display: "inline-block" }}
-            repeat={Infinity}
-          />
-        </div>
       </div>
+
       <ScrollToTop smooth />
     </section>
   );
