54c9f715b2a71380977482857bb4f5248ee3082a
Laura Vollmer <lauravollmer@Lauras-MacBook-Pro-2.local>
2024-03-06 19:09:10 -0600

    adding animation to front page

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
 import ScrollToTop from "react-scroll-to-top";
-
+import { useSpring, animated } from "react-spring";
+import { FaLaptopCode } from "react-icons/fa";
 
 const MainPage = () => {
+  const props = useSpring({
+    opacity: 2,
+    marginTop: 0,
+    from: { opacity: 0, marginTop: -500 },
+  });
   return (
     <div id="home" className="main-page">
-
       <div className="absolute top-1/2 left-1/4 transform -translate-y-1/2 -translate-x-1/4 z-10 text-black  p-4">
         <h1 className="text-8xl font-poppins">
           Software <br /> Developer.
@@ -23,11 +28,24 @@ const MainPage = () => {
           ]}
           wrapper="span"
           speed={50}
-          style={{ fontSize: "2em", display: "inline-block", color: "black", textDecoration: "bold"}}
+          style={{
+            fontSize: "2em",
+            display: "inline-block",
+            color: "black",
+            textDecoration: "bold",
+          }}
           repeat={Infinity}
-        /> 
+        />
         <h2 className="text-4xl font-poppins">websites.</h2>
-       </div>
+      </div>
+      <div className="absolute top-1/2 right-1/4 transform -translate-y-1/2 -translate-x-1/4 z-10 text-black  p-4">
+        <animated.div
+          style={props}
+          className="w-81 h-81 bg-white rounded-full shadow-lg"
+        >
+          <FaLaptopCode size={250} className="top-1/2 right-1/3 center"/>
+        </animated.div>
+      </div>
 
       <ScrollToTop smooth />
     </div>
