17e75c4a49150d23be700c991d0770301285f4c6
Laura Vollmer <lauravollmer@Lauras-MacBook-Pro-2.local>
2024-03-07 15:11:03 -0600

    styling navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,23 +3,48 @@ import { NavLink } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { AiFillLinkedin } from "react-icons/ai";
 import { FaGithubSquare } from "react-icons/fa";
+import { useState } from "react";
 
 const Navigation = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <div className="absolute top-0 flex flex-row justify-between w-full">
       <h1 className="text-3xl font-poppins p-10 font-bold">LAURA VOLLMER</h1>
       <ul className="flex flex-row right-0 p-10">
         <li className="p-2">
-          <GiHamburgerMenu size={40} className="hover:text-customColor hover:scale-110 transition duration-100" />
+          <button onClick={() => setShowMenu(!showMenu)}>
+            <GiHamburgerMenu
+              size={40}
+              className="hover:text-customColor hover:scale-110 transition duration-100"
+            />
+          </button>
         </li>
+        {showMenu && (
+          <div className="absolute top-0 right-0 bg-customColor w-1/3 h-screen">
+            <nav>
+              <ul>
+                <li>
+                  <NavLink to="/about">About</NavLink>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        )}
         <li className="p-2">
           <NavLink>
-            <AiFillLinkedin size={40} className="hover:text-customColor hover:scale-110 transition duration-100"/>
+            <AiFillLinkedin
+              size={40}
+              className="hover:text-customColor hover:scale-110 transition duration-100"
+            />
           </NavLink>
         </li>
         <li className="p-2">
           <NavLink>
-            <FaGithubSquare size={40} className="hover:text-customColor hover:scale-110 transition duration-100" />
+            <FaGithubSquare
+              size={40}
+              className="hover:text-customColor hover:scale-110 transition duration-100"
+            />
           </NavLink>
         </li>
       </ul>
