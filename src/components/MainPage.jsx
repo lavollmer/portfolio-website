1a3f50cd8d9f2de235539e2b76d57be90b4702d9
Laura Vollmer <lauravollmer@Lauras-MacBook-Pro-2.local>
2024-03-06 19:26:53 -0600

    working on mobile responsiveness

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -13,9 +13,9 @@ const MainPage = () => {
   });
 
   return (
-    <div id="home" className="main-page">
-      <div className="absolute top-1/2 left-1/4 transform -translate-y-1/2 -translate-x-1/4 z-10 text-black  p-4">
-        <h1 className="text-8xl font-poppins">
+    <>
+      <div className="text-center mt-10 md:mt-20">
+        <h1 className="text-4xl md:text-6xl font-poppins">
           Software <br /> Developer.
         </h1>
         <h2 className="text-4xl font-poppins mt-8">I like to create</h2>
@@ -31,26 +31,26 @@ const MainPage = () => {
           wrapper="span"
           speed={50}
           style={{
-            fontSize: "2em",
+            fontSize: "1em",
+            md: {fontSize: "2em"},
             display: "inline-block",
             color: "black",
             textDecoration: "bold",
           }}
           repeat={Infinity}
         />
-        <h2 className="text-4xl font-poppins">websites.</h2>
+        <h2 className="text-2xl md:text-4xl font-poppins">websites.</h2>
       </div>
-      <div className="absolute top-1/4 right-1/4 z-10 text-black  p-4">
+      <div className="absolute top-1/4 md:top-1/2 right-1/4 md:right-1/3 z-10 text-black  p-4">
         <animated.div
           style={props}
-          className="w-100 h-100 bg-white rounded-full shadow-lg"
+          className="w-64 h-64 md:w-100 md:h-100 bg-white rounded-full shadow-lg"
         >
-          <FaLaptopCode size={250} className="top-1/2 right-1/3 center"/>
+          <FaLaptopCode size={250} className="md:size-250 top-1/2 right-1/3 center" />
         </animated.div>
+        <ScrollToTop smooth />
       </div>
-
-      <ScrollToTop smooth />
-    </div>
+    </>
   );
 };
 
