59f6ac1ea86abbeeb248813f8911f031b71c454b
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-03-08 17:52:46 -0600

    playing around with styling

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -17,11 +17,11 @@ const Skills = () => {
             id="skills"
           >
             <h1 className="text-6xl font-poppins mr-8">Technical Skills</h1>
-            <p className=" sm:text-m md:text-lg p-2 leading-relaxed text-black">
+            <p className=" sm:text-m md:text-lg p-2 leading-relaxed text-black font-poppins">
               I am a dedicated and passionate professional with a strong
               background in various technical domains.
             </p>
-            <p className="sm:text-m md:text-lg p-2 w-full leading-relaxed text-black">
+            <p className="sm:text-m md:text-lg p-2 w-full leading-relaxed text-black font-poppins">
               Please review my Github profile for demonstration of skills listed
               below.
             </p>
@@ -38,7 +38,7 @@ const Skills = () => {
           <div className="flex flex-wrap -m-2">
             <div className="xl:w-1/3 md:w-1/2 p-4">
               <div className="border border-black p-6 rounded-lg">
-                <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-[#838871] text-white mb-4">
+                <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-blue text-white mb-4">
                   <IoLogoJavascript />
                 </div>
                 <h2 className="text-lg text-gray-900 font-medium title-font mb-2">
