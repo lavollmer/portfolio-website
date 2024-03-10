9ce64298c363b2955cf178fd9151b5255c42018c
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-03-10 18:10:30 -0500

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import MainPage from "./components/MainPage";
 import Footer from "./components/Footer";
-import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import About from "./components/About";
 import Work from "./components/Work";
