b0e1adacbe0935b3910c61962b918eadddb8d9fb
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-03-10 17:47:53 -0500

    updating projects

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,9 @@ import Work from "./components/Work";
 import Skills from "./components/Skills";
 import { BrowserRouter } from "react-router-dom";
 import Navigation from "./components/Navigation";
-import Background from  "../src/assets/ColorBackground.jpg";
+import Background from "../src/assets/ColorBackground.jpg";
 import ProjectDisplay from "./components/ProjectDisplay";
+import Projects from "./components/Projects";
 
 import "./App.css";
 
@@ -16,12 +17,15 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <div className=" bg-cover h-screen w-screen bg-center" style={{backgroundImage: `url(${Background})`}} >
-        {/* <div className="absolute inset-y-0 right-0 w-1/3 bg-customColor opacity-40 z-10 "></div> */}
+        <div
+          className=" bg-cover h-screen w-screen bg-center"
+          style={{ backgroundImage: `url(${Background})` }}
+        >
+          {/* <div className="absolute inset-y-0 right-0 w-1/3 bg-customColor opacity-40 z-10 "></div> */}
           <Navigation />
           <MainPage />
         </div>
-        <ProjectDisplay />
+        <Projects />
         <Skills />
         <About />
         <Work />
