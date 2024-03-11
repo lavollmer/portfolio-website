055fccaeca632a1c5b7caeed06e697622b1a25a8
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-03-11 14:59:05 -0500

    debugging postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [
+    require("postcss-import"),
+    require("postcss-preset-env")({
+      stage: 3,
+      features: {
+        "nesting-rules": true,
+      },
+    }),
+    require("autoprefixer"),
+    require("cssnano"),
+  ],
+};
