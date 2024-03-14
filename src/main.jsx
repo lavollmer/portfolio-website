3e89300b28475f816e76a74052e96096279fd961
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-03-14 12:34:21 -0500

    adding comments to main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+//import React library
+import React from "react";
+//import ReactDOM library - renders components in the DOM
+import ReactDOM from "react-dom/client";
+//import App component - JavaScript file with JSX syntax to allow HTML to be written in the file
+import App from "./App.jsx";
+//import index.css file
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+//create a new React root and render the App component
+//ReactStrictMode is a tool for highlighting potential problems in an application
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
