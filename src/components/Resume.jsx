0ae4347931b35c0f376c69039e6b9b44939e8ec9
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-03-15 12:52:45 -0500

    download resume function correct

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,9 +3,11 @@ import React from "react";
 const Resume = () => {
   return (
     <div id="resume">
-      <button className="bg-black hover:bg-[#957fef] text-white font-bold py-2 px-4 rounded">
-        Download Resume
-      </button>
+      <a href="../src/assets/resume/LauraVollmerResume2024.pdf" download>
+        <button className="bg-black hover:bg-[#957fef] text-white font-bold py-2 px-4 rounded">
+          Download Resume
+        </button>
+      </a>
     </div>
   );
 };
